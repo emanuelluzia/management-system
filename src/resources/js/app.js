a336a7fbad6ad74441e2040f385abcb931a19999
Emanuel Luzia <emanuelgobiluzia@gmailcom>
2025-08-19 23:51:09 -0300

    chore: initial Laravel 10 project setup

---
+++ src/resources/js/app.js
@@ -0,0 +1,13 @@
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { createPinia } from 'pinia'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}.vue`),
+  setup({ el, App, props, plugin }) {
+    const app = createApp({ render: () => h(App, props) })
+    app.use(plugin)
+    app.use(createPinia())
+    app.mount(el)
+  },
+})
