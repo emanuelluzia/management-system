1b963573300faeb23053fd56919e313f5d4fbc6d
Emanuel Luzia <emanuelgobiluzia@gmailcom>
2025-09-04 02:22:54 -0300

    chore(cleanup): remove old Task migration and separate Vue pages

--- src/resources/js/Pages/Tasks/Create.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Create Task</h1>
-    <form method="POST" action="/tasks">
-      <input type="text" name="title" placeholder="Task title" />
-      <button type="submit">Save</button>
-    </form>
-  </div>
-</template>
